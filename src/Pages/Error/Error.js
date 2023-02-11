6348230a8a4babcf0182c129b047ca3133b46286
Eweaa <yousefmohamed.ym2001@gmail.com>
2023-02-11 22:06:39 +0200

    UI Changes
    made UI changes according to the UI to the sign up page and the error page

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import ErrorCSS from './Error.module.css'
-import Phoenix from '../../Assets/Phoenix.png'
+import ErrorImg from '../../Assets/g10.png'
+import Polygon1 from '../../Assets/Polygon 1.png'
+import Polygon2 from '../../Assets/Polygon 2.png'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
     <div className={ErrorCSS.Error}>
-        <div>
-            <h1>Error 404</h1>
-            <h2>Page Not Found</h2>
+        <div className={ErrorCSS.LD}>
+          <img className={ErrorCSS.ErrorImg} src={ErrorImg}/>
+          <img className={ErrorCSS.Polygon1} src={Polygon1}/>
         </div>
-        <img src={Phoenix}/>
+        <div className={ErrorCSS.RD}>
+          <img className={ErrorCSS.Polygon2} src={Polygon2}/>
+          <div>
+            The requested URL was not found on this server
+            <br />
+            <Link to='/'>Back To Home</Link>
+          </div>
+        </div>
     </div>
   )
 }
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -3,6 +3,8 @@ import SignupCSS from './Signup.module.css'
 import { useAuth } from '../../Auth/AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
 import { Form, Alert, Button } from 'react-bootstrap'
+import Sign from '../../Assets/Sign.png'
+import Phoenix from '../../Assets/Phoenix.png'
 
 const Signup = () => {
 
@@ -34,31 +36,39 @@ const Signup = () => {
 
 
   return (
-    <div>
-          <h2 className='text-center mb-4'>Sign Up</h2>
+    <div className={SignupCSS.Signup}>
+      <div className={[SignupCSS.LD, 'p-5'].join(' ')}>
+        <nav className='mb-5'>
+          <img src={Phoenix} className='mx-4'/>
+          <h3>Phoenix</h3>
+        </nav>
+        <div className={SignupCSS.Form}>
+          <h2 className='mb-4'>Create An Account</h2>
           {error && <Alert variant='danger'>{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
+          <Form onSubmit={handleSubmit} style={{width:'80%'}}>
 
-            <Form.Group id='email'>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type='email' ref={emailRef} required />
+            <Form.Group id='email' className='mb-2'>
+              <Form.Control type='email' ref={emailRef} placeholder='Email' required />
             </Form.Group>
 
-            <Form.Group id='password'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef} required/>
+            <Form.Group id='password' className='mb-2'>
+              <Form.Control type='password' ref={passwordRef} placeholder='Password' required/>
             </Form.Group>
 
-            <Form.Group id='password-confirm'>
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type='password' ref={passwordConfirmRef} required/>
+            <Form.Group id='password-confirm' className='mb-2'>
+              <Form.Control type='password' ref={passwordConfirmRef} placeholder='Password Confirmation' required/>
             </Form.Group>
 
             <Button className='w-100 mt-2' type='submit' disabled={loading}>Create account</Button>
 
           </Form>
-      <div className='w-100 text-center mt-2'>
-        Already have an account? <Link to='/login'>Log In</Link>
+          <div className='w-100 text-center mt-2'>
+            Already have an account? <Link to='/login'>Log In</Link>
+          </div>
+        </div>
+      </div>
+      <div className={SignupCSS.RD}>
+        <img src={Sign}/>
       </div>
     </div>
   )
