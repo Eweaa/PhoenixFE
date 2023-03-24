f3c880fac6c17c484dc2bbf4f9c6c6f68a370d49
Eweaa <yousefmohamed.ym2001@gmail.com>
2023-03-25 00:32:26 +0200

    Update MotivationCard.js

--- src/Components/MotivationCard/MotivationCard.js
+++ src/Components/MotivationCard/MotivationCard.js
@@ -4,14 +4,7 @@ import Img from "../../Assets/doctor.jpg";
 
 const MotivationCard = (props) => {
   return (
-<<<<<<< HEAD
-    <div className={[MotivationCardCSS.MotivationCard, "p-4 mb-2"].join(" ")}>
-      <h2 className="p-1">{props.name}</h2>
-      <div className={MotivationCardCSS.Container}>
-        <div className={[MotivationCardCSS.imgContainer]}>
-          <img src={Img} alt="test" />
-=======
-    <div className={[MotivationCardCSS.MotivationCard, 'p-4 mb-2'].join(' ')}>
+    <div className={[MotivationCardCSS.MotivationCard, 'p-4 mb-3'].join(' ')}>
         <h2 className='mb-4'>{props.name}</h2>
         <div className={MotivationCardCSS.Container}>
             <div className={[MotivationCardCSS.imgContainer]}>
@@ -20,13 +13,8 @@ const MotivationCard = (props) => {
             <div className={[MotivationCardCSS.Txt, 'p-2'].join(' ')}>
                 {props.text}
             </div>
->>>>>>> a42ebe7f69d583d8bb30a4506a15a883800a738a
-        </div>
-        <div className={[MotivationCardCSS.Txt, "p-2"].join(" ")}>
-          {props.text}
         </div>
       </div>
-    </div>
   );
 };
 
