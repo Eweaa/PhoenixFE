c05ac09b0384edb1790eb3436276514856314b30
Eweaa <yousefmohamed.ym2001@gmail.com>
2023-03-28 13:46:32 +0200

    Patient Page
    Making Progress in Patient Page

--- src/Pages/DoctorPages/Patient/Patient.js
+++ src/Pages/DoctorPages/Patient/Patient.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import PatientCSS from './Patient.module.css'
+import User from '../../../Assets/GenericUser.jpg'
+import { Link } from 'react-router-dom';
 
 const Patient = () => {
   return (
-    <div>Patient</div>
+    <div className='p-4'>
+      <div className={[PatientCSS.Patient, 'p-4'].join(' ')}>
+        <div className={[PatientCSS.User, 'p-2 mb-4'].join(' ')}>
+          <img src={User}/>
+          <p style={{display:'inline'}} className='mx-2 '>
+            Ahmed
+          </p>
+        </div>
+        <div className={[PatientCSS.Nddiv, 'p-4'].join(' ')}>
+          <div className={[PatientCSS.result, 'p-2 mb-4'].join(' ')}>The patient result is “Second degree of burn”</div>
+          <button className='p-2'>Open conversation with patient</button>
+          <p>
+          We recommend <Link>this video</Link> to the patient
+
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
