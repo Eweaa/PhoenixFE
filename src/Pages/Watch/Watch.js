6ca990914ff2dba92551886c1786be52ff22b4b6
Eweaa <yousefmohamed.ym2001@gmail.com>
2023-06-23 17:38:39 +0300

    Update Watch.js

--- src/Pages/Watch/Watch.js
+++ src/Pages/Watch/Watch.js
@@ -5,6 +5,9 @@ import Vid1 from '../../Assets/vid1.mp4'
 const Watch = () => {
   return (
     <div className='p-4'>
+      <video width="750" height="500" controls >
+        <source src={Vid1} type="video/mp4"/>
+      </video>
       <Post />
       <Post />
       <Post />
